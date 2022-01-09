955bd78956264dee423281b591b6ae01d986dd1d
chris <yeditor@gmail.com>
2022-01-09 12:37:20 +0800

    這邊的 POST 比較偏向是使用記錄，需在思考

--- net/http/index.go
+++ net/http/index.go
@@ -4,32 +4,46 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
 // timeout ...
 const timeout = 5
 
-// Post example
-// @see url.URL
-// u := url.URL{
-// 	Scheme: "http",
-// 	Host: "localhost:port",
-// }
-// u.Path = "aaa/bbb" // option
-//
-// http.Post(...).Request(...)
+/*
+Post example
+
+	u := url.URL{
+		Scheme: "http",
+		Host: "localhost:port",
+		Path: "" // option
+	}
+
+	http.Post(u).Request(map[string]string{}, url.Values{})
+
+*/
 type Post url.URL
 
 // Request ...
-func (v Post) Request(p url.Values) ([]byte, error) {
+func (v Post) Request(header map[string]string, body url.Values) ([]byte, error) {
 
-	var h = &http.Client{
+	var c = &http.Client{
 		Timeout: time.Second * timeout,
 	}
-	u := url.URL(v)
-	res, e := h.PostForm(u.String(), p)
 
+	u := url.URL(v)
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(body.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	for _key, _value := range header {
+		req.Header.Set(_key, _value)
+	}
+
+	res, e := c.Do(req)
 	if e != nil {
 		return nil, e
 	}
@@ -44,16 +58,32 @@ func (v Post) Request(p url.Values) ([]byte, error) {
 	return b, nil
 }
 
-// Get example
-// http.Get("https://www.google.com/search?q=golang").Request()
-type Get string
+/*
+Get example
+
+	q := url.Values{}
+	q.Add("key", "value")
+
+	u := url.URL{
+		Scheme:   "http",
+		Host:     "localhost:port",
+		Path:     "", // option
+		RawQuery: q.Encode(),
+	}
+
+	u.Request()
+
+*/
+type Get url.URL
 
 // Request ...
 func (v Get) Request() ([]byte, error) {
 	var h = &http.Client{
 		Timeout: time.Second * timeout,
 	}
-	res, e := h.Get(string(v))
+
+	u := url.URL(v)
+	res, e := h.Get(u.String())
 
 	if e != nil {
 		return nil, e
