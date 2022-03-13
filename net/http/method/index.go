73660c5f1daea4502a825cf9bdc2864f1e44b3f0
chris <yeditor@gmail.com>
2022-03-13 10:48:09 +0800

    調整 method 方式

--- net/http/method/index.go
+++ net/http/method/index.go
@@ -1,12 +1,20 @@
 package method
 
+import "strings"
+
 // https://zh.wikipedia.org/wiki/%E5%A2%9E%E5%88%AA%E6%9F%A5%E6%94%B9
 // https://www.restapitutorial.com/lessons/httpmethods.html
 // RESTful
 const (
-	POST   = "Post"   // POST Create
-	GET    = "Get"    // GET Read
-	PUT    = "Put"    // PUT Update/Replace
-	PATCH  = "Patch"  // PATCH Update/Modify
-	DELETE = "Delete" // DELETE Delete
+	POST   classify = "Post"   // POST Create
+	GET    classify = "Get"    // GET Read
+	PUT    classify = "Put"    // PUT Update/Replace
+	PATCH  classify = "Patch"  // PATCH Update/Modify
+	DELETE classify = "Delete" // DELETE Delete
 )
+
+type classify string
+
+func (v classify) Same(t string) bool {
+	return strings.EqualFold(t, string(v))
+}
